a60a4a05db5755e6d0ffcf99eb6246c3aa2e5607
rfurlan <rfurlan@uolinc.com>
2021-12-14 22:43:38 -0300

    tf destroy dev

--- nginx.tf
+++ nginx.tf
@@ -53,7 +53,7 @@ resource "kubernetes_service" "nginx" {
       App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
     }
     port {
-      port        = 443
+      port        = 80
       target_port = 80
     }
 
